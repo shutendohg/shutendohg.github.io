5bb887d11eaf7b5531eb51f54646efb2b7e113a7
shutendohg <shutendohg@gmail.com>
2023-12-27 01:40:06 +0900

    Push templates

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://uses.craftz.dog/',
+  site: 'https://shutendohg.github.io',
   integrations: [sitemap(), react(), image(), tailwind()],
   markdown: {
     shikiConfig: {
