7aa14d4d1088891d8e4f9aa9dcac069959e988b7
shutendohg <shutendohg@gmail.com>
2025-09-08 01:27:36 +0900

    Renew my homepage using cvfolio

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'East, west, home\'s best'
-export const SITE_DESCRIPTION = 'Powered by Inkdrop & GitHub Pages'
-export const HOMEPAGE_URL = 'https://github.com/shutendohg'
